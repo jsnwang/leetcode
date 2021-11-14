19ddc8a21ea444b1c359feef0b608b18f911e32b
Jason <jwang11211@gmail.com>
2021-11-13 23:16:41 -0500

    max profit

--- java/Arrays.java
+++ java/Arrays.java
@@ -10,21 +10,7 @@ class Arrays{
         int[] a = new int[2];
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-
         for (int i = 0; i < nums.length; i++){
-
-            // naive approach
-            // O(n ^ 2)
-            // for (int j = 0 ;j < i; j++){
-            //     //System.out.println(i + " " + j);
-            //     if(nums[i] + nums[j] == target){
-            //         a = new int[]{i , j};
-            //         return a;
-            //     }
-            // }
-            
-            
-                
             if (map.containsKey(target - nums[i])){
                 a[0] = i;
                 a[1] = map.get(target - nums[i]);
@@ -36,19 +22,51 @@ class Arrays{
 
             //O(n)
             
+
+            // naive approach
+            // O(n ^ 2)
+            // for (int j = 0 ;j < i; j++){
+            //     //System.out.println(i + " " + j);
+            //     if(nums[i] + nums[j] == target){
+            //         a = new int[]{i , j};
+            //         return a;
+            //     }
+            // }
+            
             // System.out.println(map);
-            
-            
-            
             
         }
         return a;
-        // time comp: O(n^2)
 
 
     }
+
+    // 121. Best Time to Buy and Sell Stock https://leetcode.com/problems/best-time-to-buy-and-sell-stock/.
+    // You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    // You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    // Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+    public static int maxProfit(int[] prices){
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++){
+            if (min > prices[i]){
+                min = prices[i];
+
+                
+            }
+            if(prices[i] - min> max){ 
+                max = prices[i] - min;
+               
+            }
+        }
+        return max;
+    }
     public static void main (String[] args){
         int[] a = new int[]{2, 7, 11, 15};
-        twoSum(a , 9);
+        int[] a1 = new int[]{7, 1, 5, 3, 6, 4};
+
+        maxProfit(a1);
+        //twoSum(a , 9);
     }
 }
